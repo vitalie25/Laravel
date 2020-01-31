a54e406615142743f77cba0a71e80f5cc9c65eda
Railean Vitalie <vitalie657@gmail.com>
2020-01-31 11:10:13 +0200

    index php

---
+++ resources/views/index.blade.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Jumbotron Template · Bootstrap</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet" >
+    <link href="css/jumbotron.css" rel="stylesheet">
+
+     </head>
+  <body>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <div class="container">
+        <div class="navbar-header">
+           <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div class="links">
+          <a href="https://laravel.com/docs">Docs</a>
+          <a href="https://laracasts.com">Laracasts</a>
+          <a href="https://laravel-news.com">News</a>
+          <a href="https://blog.laravel.com">Blog</a>
+          <a href="https://nova.laravel.com">Nova</a>
+          <a href="https://forge.laravel.com">Forge</a>
+          <a href="https://vapor.laravel.com">Vapor</a>
+          <a href="https://github.com/laravel/laravel">GitHub</a>
+         </div>
+       </div>
+    </nav>
+
+<main role="main">
+
+  <!-- Main jumbotron for a primary marketing message or call to action -->
+  <div class="jumbotron">
+    <div class="container">
+      <h1 class="display-3">Hello, world!</h1>
+      <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+      <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+    </div>
+  </div>
+
+  <div class="container">
+    <!-- Example row of columns -->
+    <div class="row">
+      <div class="col-md-4">
+        <h2>Heading</h2>
+        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+      </div>
+      <div class="col-md-4">
+        <h2>Heading</h2>
+        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+      </div>
+      <div class="col-md-4">
+        <h2>Heading</h2>
+        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+      </div>
+    </div>
+    <hr>
+  </div> <!-- /container -->
+</main>
+<footer class="container">
+  <p>&copy; 2020 Company, Inc.</p>
+</footer>
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -6,10 +6,10 @@
 
         <title>Laravel</title>
 
-        <!-- Fonts -->
+        <!-- Fonts
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <!-- Styles -->
+        <!-- Styles 
         <style>
             html, body {
                 background-color: #fff;
@@ -98,3 +98,4 @@
         </div>
     </body>
 </html>
+-->
