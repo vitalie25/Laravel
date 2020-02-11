19769b6a6abc97870c33d0e5ff6e9b0d5b777cf4
cristi <ccazacu81@gmail.com>
2020-02-11 14:37:31 +0200

    Migrate

---
+++ database/migrations/2020_02_11_120114_create_contacts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('phone');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,5 @@
 */
 
 Route::get('/' ,'IndexController@index');
+Route::resource('contact', 'ContactController');
     
