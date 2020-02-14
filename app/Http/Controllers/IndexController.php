de2591c5d64aa311926638fbd1dde7c2304b57fc
cristi <ccazacu81@gmail.com>
2020-02-14 10:35:51 +0200

    articles

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,15 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Article;
 
 class IndexController extends Controller
 {
-    
+    //
     public function index(){
-        $hw = 'Hello World!';
-        $ms = 'This is a template for a simple marketing or informational website. It includes a large callout called a jumbatron and three supporting pieces of content. use it as a starting point to create something more unique';
-
-        return view('index')->with(['hw'=>$hw, 'ms' => $ms]);   
+        $hw='Hello world!';
+        $ms='This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.';
+        $articles=Article::select(['title', 'description', 'img', 'id', 'text'])->get();
+        return view('index')->with(['hw'=>$hw, 
+                                    'ms'=>$ms, 
+                                    'articles'=>$articles]);
+        
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -33,7 +33,7 @@
   <div class="jumbotron">
     <div class="container">
       <h1 class="display-3">{{$hw}}</h1>
-      <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+      <p>{{$ms}}</p>
       <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
     </div>
   </div>
@@ -41,22 +41,15 @@
   <div class="container">
     <!-- Example row of columns -->
     <div class="row">
+    @foreach($articles as $article)
       <div class="col-md-4">
-        <h2>Heading</h2>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+        <h2>{{$article->title}}</h2>
+        <p><img src='{{$article->img}}'</p>
+        <p>{{$article->description}}</p>
+        <p>{{$article->text}}</p>
         <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
       </div>
-      <div class="col-md-4">
-        <h2>Heading</h2>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-      </div>
-      <div class="col-md-4">
-        <h2>Heading</h2>
-        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-      </div>
-    </div>
+    @endforeach
     <hr>
   </div> <!-- /container -->
 </main>
